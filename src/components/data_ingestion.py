06ab8984e5b47090e9a1fab79a4a82735b0ebe83
ANAM <anamshaikh0515@gmail.com>
2024-07-05 12:58:57 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,9 @@
 import os
 import sys
 from src.logger import logging
+
 from src.exception import CustomException
+
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -26,15 +28,15 @@ class DataIngestion:
         logging.info('Data Ingestion method starts')
 
         try:
-            df=pd.read_csv(os.path.join('notebooks/data','gemstone.csv'))
+            data=pd.read_csv(os.path.join('notebooks/data','thyroid_data.csv'))
             logging.info('Dataset read as pandas Dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
 
             logging.info('Raw data is created')
 
-            train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
+            train_set,test_set=train_test_split(data,test_size=0.30,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
@@ -50,3 +52,11 @@ class DataIngestion:
         except Exception as e:
             logging.info('Exception occured at Data Ingestion Stage')
             raise CustomException(e,sys)
+        
+         
+        # run Data Ingestion
+        
+        
+        if __name__=='__main__':
+            obj=DataIngestion()
+            train_data,test_data=obj.initiate_data_ingestion()
