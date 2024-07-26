ca2d0ec5e245674df83e30a5a9a3424f84c2674c
ANAM <anamshaikh0515@gmail.com>
2024-07-26 12:46:43 +0530

    second commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,14 +1,21 @@
 import os
 import sys
+
+from pathlib import Path
+sys.path.append(str(Path('src').parent.parent))
+
 from src.logger import logging
 
-from src.exception import CustomException
 
+from src.exception import CustomException
+from dataclasses import dataclass
 
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
 
 ## intitialize the Data Ingestion configuration
 
@@ -19,6 +26,7 @@ class DataIngestionconfig:
     raw_data_path:str=os.path.join('artifacts','raw.csv')
 
 ## create the data ingestion class
+    
 
 class DataIngestion():
     def __init__(self):
@@ -32,6 +40,9 @@ class DataIngestion():
         try:
             data=pd.read_csv(os.path.join('notebooks/data','thyroid_data.csv'))
             logging.info('Dataset read as pandas Dataframe')
+
+    
+
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
             data.to_csv(self.ingestion_config.raw_data_path,index=False)
@@ -61,4 +72,6 @@ class DataIngestion():
         
 if __name__=='__main__':
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
